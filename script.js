5c91d87ce6e25e10c3776fd939e4428de6ca89fc
kanakSo31 <sunuwalkanak@gmail.com>
2023-06-24 19:07:46 +0530

    working on Qr.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const qrText = document.getElementById('qr-text');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const DownloadBtn = document.getElementById('DownloadBtn');
+
+const qrContainer = document.getElementById('.body');
+
+generateBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+})
