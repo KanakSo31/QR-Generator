9d9a7dc3fd6e7bfd9933fd1bc7bff1810fb4eb60
kanakSo31 <sunuwalkanak@gmail.com>
2023-06-30 13:40:10 +0530

    QR Validation

--- script.js
+++ script.js
@@ -10,18 +10,24 @@ let size = sizes.value;
 
 GenerateBtn.addEventListener('click', (e)=>{
     e.preventDefault();
+    isEmptyInput()
+    
+});
+
+sizes.addEventListener('change',(e)=>{
+    size = e.target.value;
+    isEmptyInput();
+    // generateQRCode();
+});
+
+function isEmptyInput() {
     if(qrText.value.length > 0){
 
         generateQRCode();
     }else{
         alert("Enter the text or link to Generate your QR code")
     }
-});
-
-sizes.addEventListener('change',(e)=>{
-    size = e.target.value;
-    generateQRCode();
-});
+}
 
 function generateQRCode(){
     qrContainer.innerHTML = "";
